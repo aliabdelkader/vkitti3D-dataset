b5c158a1830e23515fd15a7e2cd610cee803ede4
Ali Abdelkader <aly.abdelkader@aucegypt.edu>
2019-09-20 20:14:32 +0200

    fix paths in generate script

--- generate.sh
+++ generate.sh
@@ -1,4 +1,11 @@
-export INPUT_DIR="data"
-export OUTPUT_DIR="data"
+#!/bin/bash
+
+export INPUT_DIR="/home/fusionresearch/Datasets/vkitti3D-dataset/data"
+
+export OUTPUT_DIR="/home/fusionresearch/Datasets/vkitti3D-dataset/data"
+
 export SEQ_NUMBER="0001"
+
+cd "/home/fusionresearch/Datasets/vkitti3D-dataset/tools"
+
 python create_npy.py --root_path "${INPUT_DIR}"--out_path "${OUTPUT_DIR}" --sequence "${SEQ_NUMBER}" --v2
