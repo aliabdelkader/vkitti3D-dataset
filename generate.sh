8129ad99a99851d3d05cca322f28a070ac69ed78
Ali Abdelkader <aly.abdelkader@aucegypt.edu>
2019-09-20 22:49:09 +0200

    add converter to generate script

--- generate.sh
+++ generate.sh
@@ -7,17 +7,21 @@ export INPUT_DIR="/home/fusionresearch/Datasets/vkitti3D-dataset/data"
 
 export OUTPUT_DIR="/home/fusionresearch/Datasets/vkitti3D-dataset"
 
-export OUTPUT="${OUTPUT_DIR}/output"
+export OUTPUT_NPY="${OUTPUT_DIR}/output_npy"
+export OUTPUT_CSV="${OUTPUT_DIR}/output_csv"
 
 export SEQ_NUMBER="0001"
 
 # Set up the working environment.
 CURRENT_DIR=$(pwd)
 
-mkdir -p "${OUTPUT}"
+mkdir -p "${OUTPUT_NPY}"
+mkdir -p "${OUTPUT_CSV}"
 
 cd "/home/fusionresearch/Datasets/vkitti3D-dataset/tools" || exit
 
-python create_npy.py --root_path "${INPUT_DIR}" --out_path "${OUTPUT}" --sequence "${SEQ_NUMBER}" --v1 --njobs 4
+python create_npy.py --root_path "${INPUT_DIR}" --out_path "${OUTPUT_NPY}" --sequence "${SEQ_NUMBER}" --v1 --njobs 4
+
+python convert_npy_to_csv --input_path "${OUTPUT_NPY}" --channels 7 --output_path "${OUTPUT_CSV}"
 
 cd "${CURRENT_DIR}"
