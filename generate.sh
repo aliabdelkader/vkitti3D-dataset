52068c18e2b5b7288db5f86d9398bd4a1b42eb71
Ali Abdelkader <aly.abdelkader@aucegypt.edu>
2019-09-20 20:24:21 +0200

    add commands in generate script

--- generate.sh
+++ generate.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
 export INPUT_DIR="/home/fusionresearch/Datasets/vkitti3D-dataset/data"
 
 export OUTPUT_DIR="/home/fusionresearch/Datasets/vkitti3D-dataset/data"
 
 export SEQ_NUMBER="0001"
 
-cd "/home/fusionresearch/Datasets/vkitti3D-dataset/tools"
+# Set up the working environment.
+CURRENT_DIR=$(pwd)
 
-python create_npy.py --root_path "${INPUT_DIR}"--out_path "${OUTPUT_DIR}" --sequence "${SEQ_NUMBER}" --v2
+cd "/home/fusionresearch/Datasets/vkitti3D-dataset/tools" || exit
+
+python create_npy.py --root_path "${INPUT_DIR}" --out_path "${OUTPUT_DIR}" --sequence "${SEQ_NUMBER}" --v2
+
+cd "${CURRENT_DIR}"
