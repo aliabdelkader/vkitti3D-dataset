3c49878f651554c0d65e9aea4a16bed6927d33e0
Ali Abdelakder <aly.abdelkader@aucegypt.edu>
2019-09-20 21:35:48 +0200

    update generate script

--- generate.sh
+++ generate.sh
@@ -5,15 +5,19 @@ set -e
 
 export INPUT_DIR="/home/fusionresearch/Datasets/vkitti3D-dataset/data"
 
-export OUTPUT_DIR="/home/fusionresearch/Datasets/vkitti3D-dataset/data"
+export OUTPUT_DIR="/home/fusionresearch/Datasets/vkitti3D-dataset"
+
+export OUTPUT="${OUTPUT_DIR}/output"
 
 export SEQ_NUMBER="0001"
 
 # Set up the working environment.
 CURRENT_DIR=$(pwd)
 
+mkdir -p "${OUTPUT}"
+
 cd "/home/fusionresearch/Datasets/vkitti3D-dataset/tools" || exit
 
-python create_npy.py --root_path "${INPUT_DIR}" --out_path "${OUTPUT_DIR}" --sequence "${SEQ_NUMBER}" --v2 --njobs 1
+python create_npy.py --root_path "${INPUT_DIR}" --out_path "${OUTPUT}" --sequence "${SEQ_NUMBER}" --v2 --njobs 2
 
 cd "${CURRENT_DIR}"
